b9775dbe337180a48f12ed60911e5ccc49ae7c52
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2026-03-04 03:15:02 +0600

    Add Marquee and TuitionCategory components for dynamic tutor display

---
+++ src/components/layout/TuitionCategory.tsx
@@ -0,0 +1,11 @@
+import Marquee from './Marquee';
+
+const TuitionCategory = () => {
+  return (
+    <section className=" bg-brand-800">
+      <Marquee />
+    </section>
+  );
+};
+
+export default TuitionCategory;
