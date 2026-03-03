7f1747a8f9aeb097839c30ff55c51c99ba649ad2
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2026-03-04 03:13:55 +0600

    Add Signup route to the router configuration

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -2,6 +2,7 @@ import {createBrowserRouter} from 'react-router-dom';
 import {MainLayout} from '../components/layout';
 import Home from '../pages/Home/Home';
 import Login from '../pages/Login/Login';
+import Signup from '../pages/Signup/Signup';
 import Tuition from '../pages/Tuition/Tuition';
 
 const router = createBrowserRouter([
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
       {
         path: '/login',
         element: <Login />,
+      },
+      {
+        path: '/signup',
+        element: <Signup />,
       },
       {
         path: '/tuition',
