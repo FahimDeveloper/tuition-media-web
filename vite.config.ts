c85e75f4866a4e5fe8d3d6716f2c9fa322557432
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2026-05-02 02:55:12 +0600

    fix: ensure server configuration is correctly set with open and port options

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    open: true,
+    port: 3500,
+  },
   plugins: [
     react(),
     tailwindcss(),
