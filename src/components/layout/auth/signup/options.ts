8d22e0c726b1e2ba560f7396bc95c6727991c58b
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2026-05-13 00:57:16 +0600

    Removed unused codes

--- src/components/layout/auth/signup/options.ts
+++
@@ -1,7 +0,0 @@
-import type { AuthGender } from "@/types";
-
-export const GENDER_OPTIONS: { label: string; value: AuthGender }[] = [
-  { label: "Male", value: "male" },
-  { label: "Female", value: "female" },
-  { label: "Other", value: "other" },
-];
